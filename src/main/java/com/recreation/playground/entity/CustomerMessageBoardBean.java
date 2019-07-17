6bf1de560e193c03c035b43c45175031bfa79af2
User <User@192.168.21.156>
2019-07-17 09:33:10 +0800

    智障GIT幫我亂加HEAD  馬上修改上傳

--- src/main/java/com/recreation/playground/entity/CustomerMessageBoardBean.java
+++ src/main/java/com/recreation/playground/entity/CustomerMessageBoardBean.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.recreation.playground.entity;
 
 import java.util.Date;
@@ -47,54 +46,3 @@ public class CustomerMessageBoardBean {
 	@Column
 	private Integer customerresponseanno;
 } 
-=======
-package com.recreation.playground.entity;
-
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import lombok.Data;
-
-@Entity
-@Data
-@Table(name="CustomerMessageBoard")
-@EntityListeners(AuditingEntityListener.class)
-public class CustomerMessageBoardBean {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column
-	private int customermessageboardNum ; 
-	@Column
-	private int customermessageboardArticleFloor ; 
-	@Column 
-	private int customermessageboardResponseFloor ; 
-	@Column
-	private String customermessageboardMemberid ;
-	@Column
-	private int customermessageboardMemberNum ;
-	@Column
-	private String customermessageboardTitle ; 
-	@Column
-	private String customermessageboardMessage ; 
-	@Column(name="CustomermessageboardMessagetime")
-	@CreatedDate
-	private Date customermessageboardMessagetime;
-	@Column
-	private String customermessageboardStatus ;
-	
-	@Column
-	private Integer customerresponseanno;
-
-} 
->>>>>>> branch 'master' of https://github.com/Ryan810104/Spring.git
